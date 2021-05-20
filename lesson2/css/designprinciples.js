a92ff48c5c45ce6f75cf59e1e589a8eb26867d39
ginaestevez <est21002@byui.edu>
2021-05-19 19:27:48 -0600

    commit

--- lesson2/css/designprinciples.js
+++ lesson2/css/designprinciples.js
@@ -1,7 +1,8 @@
-document.querySelector('#lastmod').textContent = document.lastModified;
-
 const d = new Date();
 
 const currentYear = d.getFullYear();
 
+const modified = document.lastModified
+
+document.getElementById('lastModified').textContent = modified;
 document.querySelector('#currentYear').textContent = currentYear;
